1fc9a463a6660c76bd6d029b388acf31c7603e49
andrewnoyes <andrew@noyes.io>
2019-10-23 10:43:00 -0600

    convert to FC, remove unused styles, formatting

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,58 +1,50 @@
-import React from 'react'
+import React, { FunctionComponent } from "react";
 
-import { withStyles, Theme } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import posed from 'react-pose';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import posed from "react-pose";
 
-const styles: any = (theme: Theme) => ({
-    link: {
-        textDecoration: 'none'
-    },
+const useStyles = makeStyles(theme => ({
     callToAction: {
-        display: 'flex',
-        alignSelf: 'center',
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        display: "flex",
+        alignSelf: "center",
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
         boxShadow: theme.shadows[1],
         borderRadius: 3,
         border: 0,
-        color: 'white',
+        color: "white",
         height: 32,
-        padding: '0 30px',
-        transition: 'all .15s ease',
-        margin: 'auto'
+        padding: "0 30px",
+        transition: "all .15s ease",
+        margin: "auto",
     },
-    linkContainer: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        margin: 0,
-        width: 'auto'
-    }
-})
+}));
 
 const Hoverable = posed.div({
     hoverable: true,
     init: {
-        scale: 1
+        scale: 1,
     },
     hover: {
-        scale: 1.05
-    }
+        scale: 1.05,
+    },
 });
 
-class ContactButton extends React.Component<any, any> {
-    public render() {
-        const { classes, style } = this.props;
-        return (
-            <div className={classes.linkContainer}>
-                <Hoverable className={classes.hover}>
-                    <Button className={classes.callToAction} href="mailto:hello@unfrl.email" style={style || {}}>
-                        Contact
-                    </Button>
-                </Hoverable>
-            </div>
-        )
-    }
+export interface IContactButtonProps {
+    style?: React.CSSProperties;
 }
 
-export default withStyles(styles)(ContactButton);
+const ContactButton: FunctionComponent<IContactButtonProps> = props => {
+    const classes = useStyles();
+    const { style } = props;
+
+    return (
+        <Hoverable>
+            <Button className={classes.callToAction} href="mailto:hello@unfrl.email" style={style}>
+                Contact
+            </Button>
+        </Hoverable>
+    );
+};
+
+export default ContactButton;
