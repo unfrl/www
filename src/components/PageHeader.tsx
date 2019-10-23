0f39d46e02607df518e52c4cfe472131167b9ed3
andrewnoyes <andrew@noyes.io>
2019-10-23 11:52:53 -0600

    convert pageheader to FC

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,48 +1,45 @@
-import * as React from "react";
+import React, { FunctionComponent } from "react";
+import { makeStyles, Typography } from "@material-ui/core";
 
-import { withStyles, createStyles, WithStyles, Theme } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+const useStyles = makeStyles(theme => ({
+    header: {
+        width: "100%",
+        textAlign: "center",
+        marginTop: theme.spacing(4),
+        marginBottom: theme.spacing(),
+        fontSize: "2.5rem",
+        [theme.breakpoints.up("sm")]: {
+            fontSize: "3rem",
+        },
+    },
+    subtitle: {
+        width: "100%",
+        textAlign: "center",
+        fontStyle: "italic",
+    },
+    space: {
+        marginBottom: theme.spacing(4),
+    },
+}));
 
-const styles = (theme: Theme) =>
-    createStyles({
-        header: {
-            width: "100%",
-            textAlign: "center",
-            marginTop: theme.spacing(4),
-            marginBottom: theme.spacing(),
-            fontSize: "2.5rem",
-            [theme.breakpoints.up("sm")]: {
-                fontSize: "3rem",
-            },
-        },
-        subtitle: {
-            width: "100%",
-            textAlign: "center",
-            fontStyle: "italic",
-        },
-        space: {
-            marginBottom: theme.spacing(4),
-        },
-    });
-
-export interface IPageHeaderProps extends WithStyles<typeof styles> {
+export interface IPageHeaderProps {
     title: string;
     subtitle?: string;
 }
 
-class PageHeader extends React.Component<IPageHeaderProps, {}> {
-    public render() {
-        const { classes, title, subtitle } = this.props;
-        return (
-            <div>
-                <Typography variant="h1" className={classes.header}>
-                    {title}
-                </Typography>
-                {subtitle && <Typography className={classes.subtitle}>{subtitle}</Typography>}
-                <div className={classes.space}></div>
-            </div>
-        );
-    }
-}
+const PageHeader: FunctionComponent<IPageHeaderProps> = props => {
+    const classes = useStyles();
+    const { title, subtitle } = props;
 
-export default withStyles(styles)(PageHeader);
+    return (
+        <div>
+            <Typography variant="h1" className={classes.header}>
+                {title}
+            </Typography>
+            {subtitle && <Typography className={classes.subtitle}>{subtitle}</Typography>}
+            <div className={classes.space}></div>
+        </div>
+    );
+};
+
+export default PageHeader;
