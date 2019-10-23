8054ed79872b4a08f78a851fa1e666c4ca84297e
andrewnoyes <andrew@noyes.io>
2019-10-23 10:43:34 -0600

    formatting

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent } from "react";
-
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import posed from "react-pose";
