ce7ed60e771859fe7265b206fd2041d086fc39ae
Matthew Monahan <matt@kaijucode.com>
2019-11-15 21:30:53 -0700

    Oops, now i actually added it :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: `Unfrl`,
-    siteUrl: `https://unfrl.com`,
-    description: `Custom Software Design & Development`,
-    author: `Unfrl`,
-    publisher: `Unfrl`,
-    icon: 'src/assets/favicon.ico',
-    keywords: [
-      'software',
-      'development',
-      'custom',
-      'custom apps',
-      'engineering',
-      'progressive web app',
-      'PWA'
-    ]
-  },
-  plugins: [
-    `gatsby-plugin-typescript`,
-    {
-      resolve: 'gatsby-plugin-material-ui',
-      // If you want to use styled components you should change the injection order.
-      options: {
-        stylesProvider: {
-          injectFirst: true,
-        },
-      },
+    siteMetadata: {
+        title: `Unfrl`,
+        siteUrl: `https://unfrl.com`,
+        description: `Custom Software Design & Development`,
+        author: `Unfrl`,
+        publisher: `Unfrl`,
+        icon: "src/assets/favicon.ico",
+        keywords: [
+            "software",
+            "development",
+            "custom",
+            "custom apps",
+            "engineering",
+            "progressive web app",
+            "PWA",
+        ],
     },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        createLinkInHead: true
-      }
-    }
-  ],
-}
+    plugins: [
+        `gatsby-plugin-typescript`,
+        {
+            resolve: "gatsby-plugin-material-ui",
+            // If you want to use styled components you should change the injection order.
+            options: {
+                stylesProvider: {
+                    injectFirst: true,
+                },
+            },
+        },
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: `gatsby-plugin-sitemap`,
+            options: {
+                createLinkInHead: true,
+            },
+        },
+        "gatsby-plugin-offline",
+    ],
+};
