f6f0c5de56772b8bc590aa1b41168ee95210646a
Matthew Monahan <matt@kaijucode.com>
2019-11-15 21:46:33 -0700

    Simple PWA implementation (#38)
    * added gatsby-plugin-offline
    
    * Added gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,51 @@
 module.exports = {
-  siteMetadata: {
-    title: `Unfrl`,
-    siteUrl: `https://unfrl.com`,
-    description: `Custom Software Design & Development`,
-    author: `Unfrl`,
-    publisher: `Unfrl`,
-    icon: 'src/assets/favicon.ico',
-    keywords: [
-      'software',
-      'development',
-      'custom',
-      'custom apps',
-      'engineering',
-      'progressive web app',
-      'PWA'
-    ]
-  },
-  plugins: [
-    `gatsby-plugin-typescript`,
-    {
-      resolve: 'gatsby-plugin-material-ui',
-      // If you want to use styled components you should change the injection order.
-      options: {
-        stylesProvider: {
-          injectFirst: true,
-        },
-      },
+    siteMetadata: {
+        title: "Unfrl",
+        siteUrl: "https://unfrl.com",
+        description: "Custom Software Design & Development",
+        author: "Unfrl",
+        publisher: "Unfrl",
+        icon: "src/assets/favicon.ico",
+        keywords: [
+            "software",
+            "development",
+            "custom",
+            "custom apps",
+            "engineering",
+            "progressive web app",
+            "PWA",
+        ],
     },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        createLinkInHead: true
-      }
-    }
-  ],
-}
+    plugins: [
+        "gatsby-plugin-typescript",
+        {
+            resolve: "gatsby-plugin-material-ui",
+            // If you want to use styled components you should change the injection order.
+            options: {
+                stylesProvider: {
+                    injectFirst: true,
+                },
+            },
+        },
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: "gatsby-plugin-sitemap",
+            options: {
+                createLinkInHead: true,
+            },
+        },
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                name: "Unfrl",
+                short_name: "Unfrl",
+                start_url: "/",
+                background_color: "#303030",
+                theme_color: "#212121",
+                display: "standalone",
+                icon: "src/assets/logo.png",
+            },
+        },
+        "gatsby-plugin-offline",
+    ],
+};
