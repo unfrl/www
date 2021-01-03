2155a2c8b203e2656b5c2310e4d4edeee2797a20
andrewnoyes <andrew@noyes.io>
2021-01-03 11:59:03 -0700

    Try latest version of mui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
                 // stylesProvider: {
                 //     injectFirst: true,
                 // },
-                pathToStylesProvider: " ",
             },
         },
         "gatsby-plugin-react-helmet",
